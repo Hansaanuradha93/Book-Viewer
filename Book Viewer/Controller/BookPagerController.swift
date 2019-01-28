fd067531e2b176ba4e5bfdbd9828a931a5a3ed50
Hansa Anuradha <hansaanuradha@Hansas-MacBook-Pro.local>
2019-01-28 19:14:01 +0530

    Convert Colletionview cells to Pages

--- Book Viewer/Controller/BookPagerController.swift
+++ Book Viewer/Controller/BookPagerController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class BookPagerController : UICollectionViewController {
+class BookPagerController : UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     
     // MARK: - Properties
@@ -22,11 +22,22 @@ class BookPagerController : UICollectionViewController {
         // Set navigation conteoller background color
         collectionView.backgroundColor = .white
         
+        // Lets register UICollectionView cell here
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellID)
+        
+        // Lets set the Horizotal scrolling in collection view here
+        let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout
+        layout?.scrollDirection = .horizontal
+        
+        // Lets set the ability to Swipe between Pages here
+        collectionView.isPagingEnabled = true
+        
+        // Lets remove line spacing between pages here
+        layout?.minimumLineSpacing = 0
+        
         // Lets set navigation title here
         navigationItem.title = "Book"
         
-        // Lets register UICollectionView cell here
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellID)
     }
     
     // MARK: - CollectionViewController Datasource methods
@@ -38,10 +49,22 @@ class BookPagerController : UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath)
         
-        cell.backgroundColor = .red
+        if indexPath.row % 2 == 0 {
+            cell.backgroundColor = .red
+        } else {
+            cell.backgroundColor = .blue
+        }
         
         return cell
     }
+    
+    // MARK: - UICollectionViewDelegateFlowLayout Delegate methods
+    
+    // Resize the UICollectionView cell
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: view.frame.height)
+    }
+
 
     
 }
